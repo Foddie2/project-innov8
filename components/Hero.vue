d0b4ffe10581534dfdf731ffeba70e0630fa2958
Foddie2 <Bensonagala@live.com>
2021-06-08 15:25:25 +0300

    cleanups

--- components/Hero.vue
+++ components/Hero.vue
@@ -32,7 +32,6 @@ export default {};
 }
 .hero-text {
   max-width: 630px;
-  /* padding-left: 11em; */
   color: #0133a1;
 }
 .snippet {
@@ -43,42 +42,13 @@ export default {};
   width: 27.5rem;
   margin-top: 2rem;
 }
-/* .overlay {
-        background: rgba(0, 0, 0, 0);
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-    } */
 .button {
   padding: 1rem 1rem;
-  /* border-radius: 100rem; */
   background-color: #0133a1;
   color: white;
-  /* width: 8rem; */
   font-weight: 700;
   transition: 0.4s all;
 }
-
-/* .bg-image:hover .button{
-      transition: 0.5s all;
-      width: 250px;
-    }
-    .bg-image .button:hover{
-       width: 130px;
-    } */
-/* .button::after{
-    content:'';
-    position: absolute;
-    left: -20%;
-    right: -20%;
-    top:0;
-    transform: skewX(-45deg) scale(0,1);
-  }
-  .button:hover::after{
-    transform: skewX(-45deg) scale(1,1);
-  } */
-
 .button:hover {
   background-color: #e1dbdf;
   color: #0133a1;
@@ -89,9 +59,6 @@ export default {};
     min-height: 50vh;
   }
 }
-/* .hero-text {
-      margin-top: 1em;
-    } */
 .snippet {
   font-size: 1.5em;
 }
