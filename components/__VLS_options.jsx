5c5048f022bf103fb5b442aed686ead2f578be05
Foddie2 <Bensonagala@live.com>
2022-09-17 15:17:40 +0300

    first commit

---
+++ components/__VLS_options.jsx
@@ -0,0 +1,134 @@
+export default (await import('vue')).defineComponent({
+data() {
+}
+});
+const __VLS_options = {
+...({
+data() {
+}
+}),
+};
+const __VLS_name = (await import('./__VLS_types.js')).getNameOption({
+data() {
+}
+} as const);
+function __VLS_template() {
+import * as __VLS_types from './__VLS_types.js'; import('./__VLS_types.js');
+let __VLS_ctx!: __VLS_types.PickNotAny<__VLS_Ctx, {}> & InstanceType<__VLS_types.PickNotAny<typeof __VLS_component, new () => {}>> & {};
+let __VLS_vmUnwrap!: typeof __VLS_options & { components: {}; };
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_component extends { components: infer C; } ? C : {}> & __VLS_types.GlobalComponents & typeof __VLS_vmUnwrap.components & __VLS_types.PickComponents<typeof __VLS_ctx>;
+let __VLS_selfComponent!: __VLS_types.SelfComponent<typeof __VLS_name, typeof __VLS_component & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_components!: typeof __VLS_otherComponents & Omit<typeof __VLS_selfComponent, keyof typeof __VLS_otherComponents>;
+__VLS_components['/* __VLS_.SearchTexts.Components */'];
+({} as __VLS_types.GlobalAttrs)['/* __VLS_.SearchTexts.GlobalAttrs */'];
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+{
+<div class={"\u006e\u0061\u0076\u002d\u0063\u006f\u006e\u0074\u0061\u0069\u006e\u0065\u0072"}></div>;
+
+{
+<div class={"\u004e\u0061\u0076"}></div>;
+
+{
+<nav class={"\u006e\u0061\u0076\u0062\u0061\u0072\u0020\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0065\u0078\u0070\u0061\u006e\u0064\u002d\u006d\u0064\u0020\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u006c\u0069\u0067\u0068\u0074\u0020\u0062\u0067\u002d\u0077\u0068\u0069\u0074\u0065"}></nav>;
+
+{
+<button type={"\u0062\u0075\u0074\u0074\u006f\u006e"} class={"\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0074\u006f\u0067\u0067\u006c\u0065\u0072"} dataToggle={"\u0063\u006f\u006c\u006c\u0061\u0070\u0073\u0065"} data-toggle={"\u0063\u006f\u006c\u006c\u0061\u0070\u0073\u0065"} dataTarget={"\u0023\u006e\u0061\u0076\u0062\u0061\u0072\u0043\u006f\u006c\u006c\u0061\u0070\u0073\u0065"} data-target={"\u0023\u006e\u0061\u0076\u0062\u0061\u0072\u0043\u006f\u006c\u006c\u0061\u0070\u0073\u0065"}></button>;
+
+{
+<span class={"\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0074\u006f\u0067\u0067\u006c\u0065\u0072\u002d\u0069\u0063\u006f\u006e"}></span>;
+
+}
+}
+{
+<div class={"\u0063\u006f\u006c\u006c\u0061\u0070\u0073\u0065\u0020\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0063\u006f\u006c\u006c\u0061\u0070\u0073\u0065"} id={"\u006e\u0061\u0076\u0062\u0061\u0072\u004e\u0061\u0076"}></div>;
+
+{
+<a href={"\u0023"} class={"\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0062\u0072\u0061\u006e\u0064"}></a>;
+
+{
+<img class={"\u006c\u006f\u0067\u006f\u002d\u0069\u006d\u0067"} src={"\u007e\u0061\u0073\u0073\u0065\u0074\u0073\u002f\u0069\u006d\u0061\u0067\u0065\u0073\u002f\u0053\u0076\u0067\u002f\u0049\u006e\u006e\u006f\u0076\u0038\u0020\u004c\u006f\u0067\u006f\u002e\u0073\u0076\u0067"} alt={"\u004c\u006f\u0067\u006f"}></img>;
+
+}
+}
+{
+<ul class={"\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u006e\u0061\u0076\u0020\u006d\u0078\u002d\u0061\u0075\u0074\u006f"}></ul>;
+
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d\u0020\u0061\u0063\u0074\u0069\u0076\u0065"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b\u0020\u0061\u0063\u0074\u0069\u0076\u0065"} href={"\u0023\u0061\u0062\u006f\u0075\u0074"}></a>;
+
+{
+<span class={"\u0073\u0072\u002d\u006f\u006e\u006c\u0079"} fontWeightBold={true} font-weight-bold={true}></span>;
+
+}
+}
+}
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b"} href={"\u0023\u0065\u006c\u0069\u0067\u0069\u0062\u0069\u006c\u0069\u0074\u0079"}></a>;
+
+}
+}
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b"} href={"\u0023\u0062\u0065\u006e\u0065\u0066\u0069\u0074\u0073"}></a>;
+
+}
+}
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b"} href={"\u0023\u0073\u0074\u0061\u0067\u0065\u0073"}></a>;
+
+}
+}
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b"} href={"\u0023\u0066\u0061\u0071"}></a>;
+
+}
+}
+{
+<li class={"\u006e\u0061\u0076\u002d\u0069\u0074\u0065\u006d"}></li>;
+
+{
+<a class={"\u006e\u0061\u0076\u002d\u006c\u0069\u006e\u006b"} href={"\u0023\u0063\u006f\u006e\u0074\u0061\u0063\u0074\u0073"}></a>;
+
+}
+}
+}
+{
+<ul class={"\u006e\u0061\u0076\u0020\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u006e\u0061\u0076\u0020\u006e\u0061\u0076\u0062\u0061\u0072\u002d\u0072\u0069\u0067\u0068\u0074"}></ul>;
+
+{
+<svg xmlns={"\u0068\u0074\u0074\u0070\u003a\u002f\u002f\u0077\u0077\u0077\u002e\u0077\u0033\u002e\u006f\u0072\u0067\u002f\u0032\u0030\u0030\u0030\u002f\u0073\u0076\u0067"} width={"\u0031\u0036"} height={"\u0031\u0036"} fill={"\u0023\u0030\u0031\u0033\u0033\u0041\u0031"} class={"\u0062\u0069\u0020\u0062\u0069\u002d\u0070\u0065\u0072\u0073\u006f\u006e\u002d\u0066\u0069\u006c\u006c"} viewBox={"\u0030\u0020\u0030\u0020\u0031\u0036\u0020\u0031\u0036"}></svg>;
+
+{
+<path d={"\u004d\u0033\u0020\u0031\u0034\u0073\u002d\u0031\u0020\u0030\u002d\u0031\u002d\u0031\u0020\u0031\u002d\u0034\u0020\u0036\u002d\u0034\u0020\u0036\u0020\u0033\u0020\u0036\u0020\u0034\u002d\u0031\u0020\u0031\u002d\u0031\u0020\u0031\u0048\u0033\u007a\u006d\u0035\u002d\u0036\u0061\u0033\u0020\u0033\u0020\u0030\u0020\u0031\u0020\u0030\u0020\u0030\u002d\u0036\u0020\u0033\u0020\u0033\u0020\u0030\u0020\u0030\u0020\u0030\u0020\u0030\u0020\u0036\u007a"} />;
+}
+}
+}
+}
+}
+}
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+declare var __VLS_slots: {};
+return __VLS_slots;
+}
+let __VLS_component!: typeof import('./Nav.vue')['default'];
